792bcfc20ef2d719ed738213651214d59e802dd4
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2021-04-27 03:23:13 +0300

    now calling build with no keys do not copy content from BUILD except SRC. call with C key to copy content with dev build. optimised build will always copy all of the content from BUILD tho

--- chess/build.bat
+++ chess/build.bat
@@ -1,27 +1,28 @@
 echo off
 
-set SRC=src/Main.elm
+set SRC=src\Main.elm
 set OUT=main.js
-set DST=../../kotya-web/static/elm
+set DST=..\..\kotya-web\static\elm
 set BUILDPATH=build
 
 if "%1" == "o" goto :optimize
 
 :build
-  elm make %SRC% --output="%BUILDPATH%/%OUT%"
-  goto :copy
+  elm make %SRC% --output="%BUILDPATH%\%OUT%"
+  if "%2" == "c" goto :copy
+  copy "%BUILDPATH%\%OUT%" "%DST%\%OUT%" > nul
+  goto :exit
 
 :optimize
-  elm make %SRC% --output="%BUILDPATH%/%OUT%" --optimize
-  uglifyjs "%BUILDPATH%/%OUT%" --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output "%BUILDPATH%/%OUT%"
+  elm make %SRC% --output="%BUILDPATH%\%OUT%" --optimize
+  uglifyjs "%BUILDPATH%\%OUT%" --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output "%BUILDPATH%\%OUT%"
   goto :copy
 
 :copy
   xcopy %BUILDPATH% "%DST%" /y /s > nul
+  copy "..\..\kotya-web\handlers\WSChess.py" "server\WSChess.py" > nul
+  copy "..\..\kotya-web\modules\chessdb\chessdb.py" "server\chessdb.py" > nul
   goto :exit
 
 :exit
-  copy "..\..\kotya-web\handlers\WSChess.py" "server/WSChess.py" > nul
-  copy "..\..\kotya-web\modules\chessdb\chessdb.py" "server/chessdb.py" > nul
-
   echo on
